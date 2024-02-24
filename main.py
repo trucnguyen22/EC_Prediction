3b1f5aada207517b8c49afc0d035ea28e5387409
trucnguyen22 <trucnguyenytc@gmail.com>
2024-02-24 11:54:38 +0700

    changed

--- main.py
+++ main.py
@@ -29,20 +29,23 @@ st.write("Start time:", select_time[0], select_time[1])
 
 
 # Plot Data Function
-def plot_raw_data(df):
+def plot_raw_data(df, filtered_df):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.iloc[:, 0], y=df.iloc[:, 1]))
+    fig.add_trace(go.Scatter(x=filtered_df, y=df.iloc[:, 1]))
     fig.layout.update(xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 
 # Initial Data
 df = pd.read_csv('./Data/SortData.csv')
+# Filter data based on date range
+filtered_df = df[(df.index >= select_time[0]) & (df.index <= select_time[1])]
 st.header('Năng lượng tiêu thụ ban đầu')
 plot_raw_data(df)
 
-
+'''
 # Predicted Data
 df_pred = pd.read_csv('./Data/PredData.csv')
 st.header('Năng lượng tiêu thụ được dự đoán')
 plot_raw_data(df_pred)
+'''

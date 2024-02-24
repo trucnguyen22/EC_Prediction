d8fa96f695e941f633202504227268ab00eff1e1
trucnguyen22 <trucnguyenytc@gmail.com>
2024-02-24 12:52:46 +0700

    changed

--- main.py
+++ main.py
@@ -24,38 +24,40 @@ st.date_input('Date input')
 '''
 select_time = st.slider(
     "When do you start?",
-    value=(datetime(2002, 1, 1, 1, 00), datetime(2018, 8, 3, 0, 00)),)
-# format="MM/DD/YY - hh:mm")
+    value=(datetime(2002, 1, 1, 1, 00), datetime(2018, 8, 3, 0, 00)),
+    # format="MM/DD/YY - hh:mm")
+)
 st.write("Start time:", select_time[0], select_time[1])
 print(select_time[0], select_time[1])
 '''
 
 
+'''
 # Plot Data Function
 def plot_raw_data(df, filtered_df):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=filtered_df, y=df.iloc[:, 1]))
     fig.layout.update(xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
+'''
 
 
 # Initial Data
 df = pd.read_csv('./Data/SortData.csv')
 # Create Streamlit slider for selecting date range
-df['Thời gian'] = pd.to_datetime(df['Thời gian'])
-start_date = df['Thời gian'].min()
-end_date = df['Thời gian'].max()
+start_date = df['Thời gian'].min().to_pydatetime()
+end_date = df['Thời gian'].max().to_pydatetime()
 select_time = st.slider(
     "When do you start?",
-    value=[start_date, end_date])
-# format="MM/DD/YY - hh:mm")
+    value=(start_date, end_date),
+)
 st.write("Start time:", select_time[0], select_time[1])
 # Filter data based on date range
-filtered_df = df[(df['Thời gian'] >= select_time[0])
-                 & (df['Thời gian'] <= select_time[1])]
+filtered_df = df[(df['Thời gian'] >= select_time[0]) &
+                 (df['Thời gian'] <= select_time[1])]
 st.header('Năng lượng tiêu thụ ban đầu')
-print(df.index)
-plot_raw_data(df, filtered_df)
+st.write(filtered_df)
+
 
 '''
 # Predicted Data

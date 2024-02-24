6f5ee369f68c606d5309100f1c86e12870f1202d
trucnguyen22 <trucnguyenytc@gmail.com>
2024-02-24 11:04:35 +0700

    changed

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
 
-from datetime import date
+from datetime import datetime, time
 from plotly import graph_objs as go
 
 
 color_pal = sns.color_palette()
 plt.style.use('fivethirtyeight')
 st.title('Xây dựng mô hình dự đoán năng lượng tiêu thụ (Đơn vị: MegaWatt)')
+
+st.date_input('Date input')
+start_time = st.slider(
+    "When do you start?",
+    value=(datetime(2002, 1, 1, 1, 00), datetime(2018, 8, 3, 0, 00)),
+    format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
 
 
 # Plot Data Function
